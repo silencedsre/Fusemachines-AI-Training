40f76dd61eea98893616a1babcd24882efdc055f
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-12-31 17:53:12 +0545

    update script for downloading dataset

--- make_dataset.sh
+++ make_dataset.sh
@@ -1,4 +1,2 @@
 cd emotion_detection/data
-wget "https://www.floydhub.com/api/v1/resources/qM4BHN3pNjkfkvYjMMtjU4/ISEAR.csv?content=true&rename=isearcsv"
-file_name=$(ls | grep ISEAR)
-mv $file_name ISEAR.csv
+wget -O ISEAR.csv "https://www.floydhub.com/api/v1/resources/qM4BHN3pNjkfkvYjMMtjU4/ISEAR.csv?content=true&rename=isearcsv"
