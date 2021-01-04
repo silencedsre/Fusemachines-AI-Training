d13887b584e409fa5ce9cdeb7e5c099f6c4b9f56
silencedsre <jamakatel.shree.krishna@gmail.com>
2021-01-04 20:56:51 +0545

    update baseline.py

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -3,7 +3,7 @@ import pandas as pd
 from config.config import DATA_PATH
 from src.data.preprocess_data import preprocess_data
 from src.features.vectorize import split_dataset, vectorize
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, classification_report
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.multiclass import OneVsRestClassifier
 
@@ -13,12 +13,13 @@ def multinomial_nv_clf(X_train_vec, X_test_vec, y_train, y_test):
     clf.fit(X_train_vec, y_train)
     y_pred = clf.predict(X_test_vec)
     score = f1_score(y_test, y_pred, average='macro', labels=np.unique(y_pred))
-    return score
+    clf_rep = classification_report(y_test, y_pred)
+    return score, clf_rep
 
 if __name__ == "__main__":
     df = pd.read_csv(DATA_PATH, header=None)
     df = preprocess_data(df)
     X_train, X_test, y_train, y_test = split_dataset(df)
     X_train_vec, X_test_vec = vectorize(X_train, X_test)
-    score = multinomial_nv_clf(X_train_vec, X_test_vec, y_train, y_test)
-    print(score)
+    score, clf_rep = multinomial_nv_clf(X_train_vec, X_test_vec, y_train, y_test)
+    print(clf_rep)
