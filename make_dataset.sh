521c8518e0ba11443386c52ea1b988fff7c1fbe0
silencedsre <jamakatel.shree.krishna@gmail.com>
2021-01-04 07:29:45 +0545

    add script for downloading kaggle car ads dataset

--- make_dataset.sh
+++ make_dataset.sh
@@ -1,2 +1,5 @@
 cd emotion_detection/data
 wget -O ISEAR.csv "https://www.floydhub.com/api/v1/resources/qM4BHN3pNjkfkvYjMMtjU4/ISEAR.csv?content=true&rename=isearcsv"
+kaggle datasets download -d mirosval/personal-cars-classifieds
+unzip -o personal-cars-classifieds
+rm -rf personal-cars-classifieds.zip
