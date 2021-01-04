16b8e7adc4dd4488f23e8b40c6850be4dfa89315
silencedsre <jamakatel.shree.krishna@gmail.com>
2021-01-04 18:41:00 +0545

    update config.py

--- config/config.py
+++ config/config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATA_PATH = Path.joinpath(BASE_DIR, "emotion_detection", "data", "ISEAR.csv")
+DATA_PATH = Path.joinpath(BASE_DIR, "data", "ISEAR.csv")
 
 DATABASE_USERNAME = ""
 DATABASE_PASSWORD = ""
