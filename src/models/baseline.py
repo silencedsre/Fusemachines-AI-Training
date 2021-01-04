bdf228019594ee01865f02a16e57508d55592754
silencedsre <jamakatel.shree.krishna@gmail.com>
2021-01-04 20:25:27 +0545

    add baseline multinomial naive bayes

---
+++ src/models/baseline.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from config.config import DATA_PATH
+from src.data.preprocess_data import preprocess_data
+from src.features.vectorize import split_dataset, vectorize
+from sklearn.metrics import f1_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.multiclass import OneVsRestClassifier
+
+
+def multinomial_nv_clf(X_train_vec, X_test_vec, y_train, y_test):
+    clf = OneVsRestClassifier(MultinomialNB())
+    clf.fit(X_train_vec, y_train)
+    y_pred = clf.predict(X_test_vec)
+    score = f1_score(y_test, y_pred, average='macro', labels=np.unique(y_pred))
+    return score
+
+if __name__ == "__main__":
+    df = pd.read_csv(DATA_PATH, header=None)
+    df = preprocess_data(df)
+    X_train, X_test, y_train, y_test = split_dataset(df)
+    X_train_vec, X_test_vec = vectorize(X_train, X_test)
+    score = multinomial_nv_clf(X_train_vec, X_test_vec, y_train, y_test)
+    print(score)
