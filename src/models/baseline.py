34c53ef5a138618d757f2da6999e40123ae2482f
anilsah895 <anilsah895@gmail.com>
2021-01-05 19:35:40 +0545

    RandomForest classifier

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -4,12 +4,13 @@ from config.config import DATA_PATH
 from src.data.preprocess_data import preprocess_data
 from src.features.vectorize import split_dataset, vectorize
 from sklearn.metrics import f1_score, classification_report
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.multiclass import OneVsRestClassifier
+# from sklearn.naive_bayes import MultinomialNB
+# from sklearn.multiclass import OneVsRestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
-
-def multinomial_nv_clf(X_train_vec, X_test_vec, y_train, y_test):
-    clf = OneVsRestClassifier(MultinomialNB())
+def randomforest_clf(X_train_vec, X_test_vec, y_train, y_test):
+    clf = RandomForestClassifier(n_estimators=100, random_state=0)
+    # clf = OneVsRestClassifier(MultinomialNB())
     clf.fit(X_train_vec, y_train)
     y_pred = clf.predict(X_test_vec)
     score = f1_score(y_test, y_pred, average='macro', labels=np.unique(y_pred))
@@ -21,5 +22,5 @@ if __name__ == "__main__":
     df = preprocess_data(df)
     X_train, X_test, y_train, y_test = split_dataset(df)
     X_train_vec, X_test_vec = vectorize(X_train, X_test)
-    score, clf_rep = multinomial_nv_clf(X_train_vec, X_test_vec, y_train, y_test)
+    score, clf_rep = randomforest_clf(X_train_vec, X_test_vec, y_train, y_test)
     print(clf_rep)
