80282f10b7fc19b134a56571a724b3f3425c4c48
anilsah895 <anilsah895@gmail.com>
2021-01-07 19:58:50 +0545

    flask get and post

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH = Path.joinpath(BASE_DIR, "data", "ISEAR.csv")
 VECTORIZER_PATH = Path.joinpath(BASE_DIR, "temp", "vectorizer")
-MODEL_PATH = Path.joinpath(BASE_DIR, "models", "naive_bayes", "model.pkl")
+MODEL_PATH = Path.joinpath(BASE_DIR, "models", "randomforest", "model.pkl")
 
 DATABASE_USERNAME = ""
 DATABASE_PASSWORD = ""
