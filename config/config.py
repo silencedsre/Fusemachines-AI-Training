0a61f365f56255ecd053d43111627a70387123e7
silencedsre <jamakatel.shree.krishna@gmail.com>
2021-01-08 07:47:04 +0545

    create data path

--- config/config.py
+++ config/config.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = Path(BASE_DIR).joinpath("data")
+Path(DATA_DIR).mkdir(exist_ok=True)
 DATA_PATH = Path.joinpath(BASE_DIR, "data", "ISEAR.csv")
 VECTORIZER_PATH = Path.joinpath(BASE_DIR, "temp", "vectorizer")
 MODEL_PATH = Path.joinpath(BASE_DIR, "models", "naive_bayes", "model.pkl")
